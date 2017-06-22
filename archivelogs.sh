a2eaf967c5196558b25b971b6436b67f9b2462d9
Ruwan Ranganath <Ruwan-Ranganath@users.noreply.github.com>
2017-06-22 13:57:49 +0530

    updated

--- archivelogs.sh
+++ archivelogs.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-currentlogpath = /mnt/10.33.221.172/wso2as-5.3.0/logs
-archivedlogpath = /mnt/archivedlogs
+NOW=$(date +"%d-%m-%Y")
 
-cd $currentlogpath 
+currentlogpath=/mnt/10.33.221.172/wso2as-5.3.0/logs
+archivedlogpath=/mnt/archivedlogs
 
-touch logarchiving-log.txt
-
-echo pwd >>> logarchiving-log.txt
+cd $currentlogpath
+sudo touch logarchiving-log.txt
+pwd >> logarchiving-log.txt 
 
 bzip2 *2017-06-1*
 
 mv *.bz2 $archivedlogs
 
-find $archivedlogs -mtime 300 -type f -delete >> logarchiving-log.txt 
-
-
+find $archivedlogs -mtime 300 -type f -delete >> logarchiving-log.txt
